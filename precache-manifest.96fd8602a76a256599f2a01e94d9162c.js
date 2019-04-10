5a2cd2f3aa480f278b2ac88e79ec6ac29c642e9b
Blac-Panda <oyefesotunmise@gmail.com>
2019-04-10 16:22:26 +0100

    first commit

---
+++ precache-manifest.96fd8602a76a256599f2a01e94d9162c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/cregital/static/media/search.c6a0effb.svg"
+  },
+  {
+    "revision": "a7a599f2633486bdcb4c",
+    "url": "/cregital/static/js/runtime~main.eda2a3d1.js"
+  },
+  {
+    "revision": "ca6f7a8f70b8639748b8",
+    "url": "/cregital/static/js/main.22b2885a.chunk.js"
+  },
+  {
+    "revision": "1a04be1bc217ccb4a34f",
+    "url": "/cregital/static/js/2.c6a75f39.chunk.js"
+  },
+  {
+    "revision": "ca6f7a8f70b8639748b8",
+    "url": "/cregital/static/css/main.90f92579.chunk.css"
+  },
+  {
+    "revision": "b1be16925073c3aff897fa165e57e132",
+    "url": "/cregital/index.html"
+  }
+];
